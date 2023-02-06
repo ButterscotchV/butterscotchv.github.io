801a4cb01dcf4e5bb4171a1cbe7480347772b95b
Butterscotch! <bscotchvanilla@gmail.com>
2023-02-06 14:20:41 -0500

    Clean up dark mode detection

--- _includes/js/custom.js
+++ _includes/js/custom.js
@@ -1,12 +1,15 @@
-window.matchMedia("(prefers-color-scheme: dark)")
-  .addEventListener('change', event => {
+if (window.matchMedia) {
+  const darkModeMedia = window.matchMedia("(prefers-color-scheme: dark)");
+
+  if (darkModeMedia.matches) {
+    jtd.setTheme("dark");
+  }
+
+  darkModeMedia.addEventListener("change", event => {
     if (event.matches) {
       jtd.setTheme("dark");
     } else {
       jtd.setTheme("light");
     }
   });
-
-if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-  jtd.setTheme("dark");
 }
